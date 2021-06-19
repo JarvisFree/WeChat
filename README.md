86ad3676ced69db91a448021d8a21aa3781a2670
维斯 <zhanggxcpp@163.com>
2021-06-19 15:48:56 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,69 @@
+# README
+
+微信消息自动化
+
+## Table of Contents
+
+- [Background](#background)
+- [Install](#install)
+- [Usage](#usage)
+  - [Generator](#generator)
+- [Badge](#badge)
+- [Example Readmes](#example-readmes)
+- [Related Efforts](#related-efforts)
+- [Maintainers](#maintainers)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Background
+
+女朋友不回消息时 轰炸她 哈哈（唯一的结果：拉黑）
+
+## Install
+
+****
+
+| 工具         | 版本     | 备注                      |
+|:------------|:---------|:------------------------|
+| PyCharm工具  | 2021.1.1 |                         |
+| Python工具   | 3.9.5    |                         |
+| PyWin32     | 301      | pyperclip的使用           |
+| pyperclip   | 1.8.2    | 需先按照PyWin32           |
+| PyUserInput | 0.1.10   | pymouse、pykeyboard的使用 |
+
+****
+
+
+## Usage
+
+暂无
+
+### Generator
+
+暂无
+
+## Badge
+
+暂无
+
+## Example Readmes
+
+视频效果展示  
+[微信_消息自动化.mp4](video/微信_消息自动化.mp4)
+
+## Related Efforts
+
+暂无
+
+## Maintainers
+
+暂无
+
+## Contributing
+
+暂无
+
+## License
+
+[博客](https://www.cnblogs.com/danhuai) |
+[GitHub](https://github.com/JarvisFree) | © 维斯
---
+++ we_chat/auto_message/send_message.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    ：2021/6/19 14:45
+@Author  ：维斯
+@File    ：send_message.py
+@Version ：1.0
+@Function：在当前窗口自动发送消息
+"""
+
+import random
+import time
+import pyperclip
+from pymouse import *
+from pykeyboard import PyKeyboard
+
+
+def read_data(file_path):
+    data = []
+    for line in open(file_path, 'r', encoding='utf-8'):
+        if line.endswith('\n'):
+            data.append(line[:-1])
+        else:
+            data.append(line)
+    # print('11111111111111111')
+    # print(*data, sep='\n')
+    # print('11111111111111111')
+    return data
+
+
+def start(data_file, min_time, max_time, first_time=10):
+    """
+    开始执行
+    :param data_file: 数据文件
+    :param min_time: 最小发送间隔时间（s）
+    :param max_time: 最大发送间隔时间（s）
+    :param first_time: 首次运行等待时间（s）
+    """
+    k = PyKeyboard()
+
+    data_list = read_data(data_file)
+    time.sleep(first_time)
+    count = 1
+    while True:
+        data_str = random.choice(data_list)
+        pyperclip.copy(data_str)
+        time_num = random.randint(min_time, max_time)
+        print(f'第{count}条({time_num}s)：{data_str}')
+        k.press_key(k.control_key)
+        k.tap_key('v')
+        k.release_key(k.control_key)
+        time.sleep(time_num)
+        k.tap_key(k.enter_key)
+        count += 1
+
+
+if __name__ == '__main__':
+    start('data/data.txt', 1, 10)
